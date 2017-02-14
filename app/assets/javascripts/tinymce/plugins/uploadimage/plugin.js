84f2e137f06a6fb15f255561c798b9e442eba92e
manoj-netlings <manoj@netlingshq.com>
2017-02-14 18:46:00 +0530

    Added Textbox for class

--- app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
+++ app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
@@ -18,6 +18,7 @@
             {type: 'iframe',  url: 'javascript:void(0)'},
             {type: 'textbox', name: 'file', label: ed.translate('Choose an image'), subtype: 'file'},
             {type: 'textbox', name: 'alt',  label: ed.translate('Image description')},
+            {type: 'textbox', name: 'class',  label: ed.translate('Image class')},
             {type: 'container', classes: 'error', html: "<p style='color: #b94a48;'>&nbsp;</p>"},
 
             // Trick TinyMCE to add a empty div that "preloads" the throbber image
